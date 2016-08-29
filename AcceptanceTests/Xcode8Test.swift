dd05d1caecd0c127649d3cba1f735100c6c58b06
Shashikant86 <shashikant.jagtap@icloud.com>
2016-08-29 12:02:32 +0100

    Implented sample test

---
+++ AcceptanceTests/Xcode8Test.swift
@@ -0,0 +1,26 @@
+import Foundation
+@objc(Xcode8Test)
+
+class Xcode8Test: NSObject, SlimDecisionTable {
+
+    var input = ""
+
+    func setInput() -> String {
+
+        return "xcode8"
+    }
+
+
+    func execute() {
+
+    }
+
+    func output() -> String {
+
+        return "pass"
+    }
+
+
+
+
+}
