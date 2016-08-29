36937b6c1833a857e9a2ff671b03056a8ef0fb8b
Shashikant86 <shashikant.jagtap@icloud.com>
2016-08-29 11:08:47 +0100

    Support for Fitnesse Acceptance Tests

--- OCSlimProjectTestRunner.sh
+++ OCSlimProjectTestRunner.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/Users/shashi/Library/Developer/Xcode/DerivedData/SierraXCFit-fnkgphiddzsbenafljoinzudtmyn/Build/Products/Debug-iphonesimulator/AcceptanceTests.app/OCSlimProject.bundle/RunTestsTargetWithSlimPort $1
+/Users/shashi/Library/Developer/Xcode/DerivedData/SierraXCFit-dezucsodeavxnlgitnfhqqvdjclf/Build/Products/Debug-iphonesimulator/AcceptanceTests.app/OCSlimProject.bundle/RunTestsTargetWithSlimPort $1
